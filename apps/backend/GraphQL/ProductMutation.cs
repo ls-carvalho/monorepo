354e6c3e8989cb9049447a6543ec7ed87a21e73a
Lucas Carvalho <lucas.carvalho@itixti.com.br>
2024-06-26 15:52:46 -0300

    Added implementation for ProductService

--- apps/backend/GraphQL/ProductMutation.cs
+++ apps/backend/GraphQL/ProductMutation.cs
@@ -7,12 +7,15 @@ namespace Monorepo.Backend.GraphQL;
 public class ProductMutation
 {
   [GraphQLName("createProduct")]
+  [UseProjection]
   public async Task<Product> CreateProductAsync(CreateProductDto product, [Service] IProductService productService) => await productService.CreateProductAsync(product);
 
   [GraphQLName("updateProduct")]
+  [UseProjection]
   public async Task<Product> UpdateProductAsync(UpdateProductDto product, [Service] IProductService productService) => await productService.UpdateProductAsync(product);
 
   [GraphQLName("deleteProduct")]
+  [UseProjection]
   public async Task<Product> DeleteProductAsync(long id, [Service] IProductService productService) => await productService.DeleteProductAsync(id);
 
 }
