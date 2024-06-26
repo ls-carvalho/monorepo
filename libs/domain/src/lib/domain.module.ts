5745fcc1737aa784341e0800f3d0d845e21f76f5
Lucas Carvalho <lucas.carvalho@itixti.com.br>
2024-06-26 10:49:24 -0300

    Added grid and form components to lib

--- libs/domain/src/lib/domain.module.ts
+++ libs/domain/src/lib/domain.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GridComponent } from './grid/grid.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [GridComponent, FormComponent],
 })
 export class DomainModule {}
---
+++ libs/domain/src/lib/form/form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'monorepo-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss'],
+})
+export class FormComponent {}
