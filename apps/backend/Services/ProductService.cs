09ff806d6069c115eb15eb31971648a48f3f54df
Lucas Carvalho <lucas.carvalho@itixti.com.br>
2024-06-26 15:52:54 -0300

    Added Impl.

--- apps/backend/Services/ProductService.cs
+++ apps/backend/Services/ProductService.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using Monorepo.Backend.Context;
 using Monorepo.Backend.Context.Entities;
 using Monorepo.Backend.DataTransferObject;
 using Monorepo.Backend.Services.Interfaces;
@@ -5,18 +7,54 @@ using Monorepo.Backend.Services.Interfaces;
 namespace Monorepo.Backend.Services;
 public class ProductService : IProductService
 {
-  public Task<Product> CreateProductAsync(CreateProductDto product)
+  private readonly ProductContext _productContext;
+
+  public ProductService(ProductContext productContext)
   {
-    throw new NotImplementedException();
+    _productContext = productContext;
   }
 
-  public Task<Product> DeleteProductAsync(long id)
+  public async Task<Product> CreateProductAsync(CreateProductDto product)
   {
-    throw new NotImplementedException();
+    var newProduct = new Product
+    {
+      Name = product.Name,
+      Value = product.Value,
+    };
+
+    await _productContext.Products.AddAsync(newProduct);
+    await _productContext.SaveChangesAsync();
+
+    return newProduct;
   }
 
-  public Task<Product> UpdateProductAsync(UpdateProductDto product)
+  public async Task<Product> DeleteProductAsync(long id)
   {
-    throw new NotImplementedException();
+    var entity = await _productContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+    if (entity is null)
+    {
+      throw new ArgumentException("Product does not exist.");
+    }
+
+    await Task.Run(() => _productContext.Products.Remove(entity));
+    await _productContext.SaveChangesAsync();
+
+    return entity;
+  }
+
+  public async Task<Product> UpdateProductAsync(UpdateProductDto product)
+  {
+    var entity = await _productContext.Products.FirstOrDefaultAsync(p => p.Id == product.Id);
+    if (entity is null)
+    {
+      throw new ArgumentException("Product does not exist.");
+    }
+
+    entity.Name = product.Name;
+    entity.Value = product.Value;
+
+    await _productContext.SaveChangesAsync();
+
+    return entity;
   }
 }
