d11bffa11b9614e77861e42d27cecb48ae019da5
Lucas Carvalho <lucas.carvalho@itixti.com.br>
2024-06-28 12:48:51 -0300

    Fix to backend CORS and https port.

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -4,14 +4,26 @@ using Monorepo.Backend.GraphQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddServices();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddContext(builder.Configuration);
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureGraphQL();
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowLocal",
+    policy => policy
+      .AllowAnyHeader()
+      .AllowAnyMethod()
+      .AllowCredentials()
+      .SetIsOriginAllowed(url => true)
+    );
+});
 
 var app = builder.Build();
 
+app.UseCors("AllowLocal");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
